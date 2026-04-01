db298a6675f2032a8afb0b6fa25f5cd5412d9f06
Tudy0406 <radtudor7@gmail.com>
2026-04-01 10:24:48 +0300

    feat(Main, config): final touch
    - a version of the application properties configuration, but it can be modified by the user for the system to perform differently at runtime
    - connected all the components in Main for easy application run

--- file_search_engine/src/main/java/com/search/Main.java
+++ file_search_engine/src/main/java/com/search/Main.java
@@ -1,30 +1,67 @@
 package com.search;
+import com.search.config.ConfigLoader;
 import com.search.db.DatabaseManager;
 import com.search.db.SQLiteDatabaseManager;
-import com.search.sync.FileCrawler;
-import com.search.sync.SimpleFileCrawler;
-
-import java.nio.file.Path;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.util.List;
-
-import java.nio.file.Path;
-import java.util.Set;
+import com.search.report.IndexReport;
+import com.search.service.QueryParser;
+import com.search.service.ResultFormatter;
+import com.search.service.SearchDatabaseAdapter;
+import com.search.service.SearchService;
+import com.search.sync.*;
+import com.search.ui_cli.*;
 
 public class Main {
     public static void main(String[] args) {
 
+        DatabaseManager dbManager = new SQLiteDatabaseManager();
+        dbManager.connect();
+        dbManager.initializeSchema();
+
+        // SYNC
+        ConfigLoader configLoader = new ConfigLoader("C:/Users/Tudor/OneDrive - Technical University of Cluj-Napoca/Desktop/facultate/year_3/SD/file_search_engine_project/file_search_engine/src/main/java/com/search/config/app.properties");
         FileCrawler crawler = new SimpleFileCrawler();
+        FileReader reader = new FileReader();
+        MetadataExtractor extractor = new MetadataExtractor();
+        DatabaseAdapter syncDb = new DatabaseAdapter(dbManager);
 
-        Set<String> excluded = Set.of(".jpg", ".png", ".exe", ".ogg");
+        SyncService syncService = new SyncService(configLoader, crawler, reader, extractor, syncDb);
 
-        var files = crawler.crawl(
-                Path.of("C:/Users/Tudor/OneDrive - Technical University of Cluj-Napoca/Desktop/random"),
-                excluded
-        );
+        // SEARCH
+        QueryParser parser = new QueryParser();
+        SearchDatabaseAdapter searchDb = new SearchDatabaseAdapter(dbManager);
+        ResultFormatter formatter = new ResultFormatter();
+        SearchService searchService = new SearchService(parser, searchDb, formatter);
 
-        files.forEach(System.out::println);
+        // UI
+        InputHandler input = new InputHandler();
+        View view = new View();
+
+        view.showWelcome();
+        view.showIndexingStart();
+
+        IndexReport indexReport = syncService.sync();
+        indexReport.print();
+
+
+        // MAIN LOOP
+        while (true) {
+
+            String query = input.readQuery();
+
+            if (query.equalsIgnoreCase("exit")) {
+                view.showExit();
+                break;
+            }
+
+            if (query.isBlank()) {
+                view.showEmptyQuery();
+                continue;
+            }
+
+            searchService.search(query);
+        }
+
+        dbManager.close();
     }
 }
 
