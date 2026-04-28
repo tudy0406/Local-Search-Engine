b30d915f7aa7cb02933f7f1cc2aa155b9d3ce5d5
Tudy0406 <radtudor7@gmail.com>
2026-04-28 22:00:48 +0300

    feat(spellcorrection): update database manager to adapt to new spelling correction feature
    -add 2 new tables: word_frequency -> holds all the unique words from indexed files content and their frequency
                       file_word_count -> holds all the words and their frequencies in an indexed file

--- file_search_engine/src/main/java/com/search/db/DatabaseManager.java
+++ file_search_engine/src/main/java/com/search/db/DatabaseManager.java
@@ -1,10 +1,19 @@
 package com.search.db;
 
+import java.nio.file.Path;
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Map;
 
 public interface DatabaseManager {
     void connect();
     Connection getConnection();
     void initializeSchema();
     void close();
+    Map<String, Integer> loadWordFrequencies() throws SQLException;
+    Map<String, Integer> getFileWordCount(Path path) throws SQLException;
+    void insertOrUpdateWord(String word, int count) throws SQLException;
+    void insertFileWord(String path, String word, int count) throws SQLException;
+    void deleteFileWords(String path) throws SQLException;
+
 }
--- file_search_engine/src/main/java/com/search/db/SQLiteDatabaseManager.java
+++ file_search_engine/src/main/java/com/search/db/SQLiteDatabaseManager.java
@@ -1,6 +1,9 @@
 package com.search.db;
 
+import java.nio.file.Path;
 import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SQLiteDatabaseManager implements DatabaseManager {
 
@@ -68,6 +71,27 @@ public class SQLiteDatabaseManager implements DatabaseManager {
                 END;
             """);
 
+            stmt.execute("""
+                CREATE TABLE IF NOT EXISTS word_frequency (
+                    word TEXT PRIMARY KEY,
+                    count INTEGER NOT NULL
+                );
+            """);
+
+            stmt.execute("""
+                CREATE TABLE IF NOT EXISTS file_word_count (
+                    file_path TEXT,
+                    word TEXT,
+                    count INTEGER,
+                    PRIMARY KEY (file_path, word)
+                );
+            """);
+
+            stmt.execute("""
+            CREATE INDEX IF NOT EXISTS idx_file_word_path ON file_word_count(file_path);
+            CREATE INDEX IF NOT EXISTS idx_word_frequency_word ON word_frequency(word);
+            """);
+
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -82,14 +106,89 @@ public class SQLiteDatabaseManager implements DatabaseManager {
         }
     }
 
-    public void addColumns(String table, String[] columns, String[] types){
-        try(Statement stmt = connection.createStatement()){
-            for(int i = 0; i < columns.length; i++){
-                String sql = String.format("ALTER TABLE %s ADD COLUMN IF NOT EXISTS %s %s", table, columns[i], types[i]);
-                stmt.execute(sql);
+    public Map<String, Integer> loadWordFrequencies() throws SQLException {
+        Map<String, Integer> wordFreq = new HashMap<>();
+
+        String sql = "SELECT word, count FROM word_frequency";
+
+        try (PreparedStatement stmt = connection.prepareStatement(sql);
+             ResultSet rs = stmt.executeQuery()) {
+
+            while (rs.next()) {
+                String word = rs.getString("word");
+                int count = rs.getInt("count");
+
+                wordFreq.put(word, count);
             }
-        }catch(SQLException e){
-            System.err.println("Error inserting columns.");
+        }
+
+        return wordFreq;
+    }
+
+    public Map<String, Integer> getFileWordCount(Path path) throws SQLException {
+        Map<String, Integer> wordFreq = new HashMap<>();
+
+        String sql = "SELECT word, count FROM file_word_count WHERE file_path = ?";
+
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setString(1, String.valueOf(path));
+
+            try (ResultSet rs = stmt.executeQuery()) {
+                while (rs.next()) {
+                    String word = rs.getString("word");
+                    int count = rs.getInt("count");
+
+                    wordFreq.put(word, count);
+                }
+            }
+        }
+
+        return wordFreq;
+    }
+
+
+
+    public void insertOrUpdateWord(String word, int delta) throws SQLException {
+        String sql = """
+        INSERT INTO word_frequency(word, count)
+        VALUES (?, ?)
+        ON CONFLICT(word)
+        DO UPDATE SET count = count + excluded.count
+    """;
+
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setString(1, word);
+            stmt.setInt(2, delta);
+            stmt.executeUpdate();
+        }
+
+        String cleanup = "DELETE FROM word_frequency WHERE count <= 0";
+
+        try (PreparedStatement stmt = connection.prepareStatement(cleanup)) {
+            stmt.executeUpdate();
+        }
+    }
+
+    public void insertFileWord(String path, String word, int count) throws SQLException {
+        String sql = """
+        INSERT INTO file_word_count(file_path, word, count)
+        VALUES (?, ?, ?)
+    """;
+
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setString(1, path);
+            stmt.setString(2, word);
+            stmt.setInt(3, count);
+            stmt.executeUpdate();
+        }
+    }
+
+    public void deleteFileWords(String path) throws SQLException {
+        String sql = "DELETE FROM file_word_count WHERE file_path = ?";
+
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setString(1, path);
+            stmt.executeUpdate();
         }
     }
 }
